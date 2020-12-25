9e82a85f1a6245d917c7980a03fb3a92b29baed8
krishnamoorthy <krishnamoorthy980@gmail.com>
2020-12-25 18:38:48 +0530

    commit gitaddx

--- src/main/java/sampleproject/browser.java
+++ src/main/java/sampleproject/browser.java
@@ -13,6 +13,7 @@ public class browser {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com/");
 		driver.get("this is add column");
+		driver.get("this is gitaddx1 column");
 	}
 	
 	@Test
@@ -22,6 +23,7 @@ public class browser {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com/");
 		driver.get("this is add column");
+		driver.get("this is gitaddx1 column");
 	}
 
 }
